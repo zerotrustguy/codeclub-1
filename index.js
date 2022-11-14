40dd35c5b64f7f88180d35133f66da59f19de206
zerotrustguy <sheril@cloudflare.com>
2022-11-14 00:01:28 +0000

    final

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 export default {
-	fetch() {
-		return new Response('Hello world !from sheril', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+	fetch(request) {
+		if(request.url =="https://0951835b7ba44ba7977ff97e00eb0eb1.edgecorp.workers.dev/") {
+			return new Response('Hello worker!', {
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
+		else{
+			return new Response('Error Worker!', {
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
 	},
-};
+ };
+ 
+
+
